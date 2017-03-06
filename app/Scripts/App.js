b9d7b4e96cb00793e8a892947df12942fafd29a5
Eddie <eddiejudehareven@gmail.com>
2017-03-06 12:31:37 -0500

    stickyheader

---
+++ app/Scripts/App.js
@@ -0,0 +1,7 @@
+import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/reveal-on-scroll';
+import StickyHeader from './modules/StickyHeader'; 
+
+var mobileMenu = new MobileMenu(); /* var = object = new = person class */
+var revealOnScroll = new RevealOnScroll();
+var stickyHeader = new StickyHeader();
