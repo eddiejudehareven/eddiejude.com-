3227a4a68f0b14fd835aa0fc11ff66cd26359423
Eddie <eddiejudehareven@gmail.com>
2017-04-17 13:02:07 -0400

    made changes to navigation bar

--- app/Scripts/App.js
+++ app/Scripts/App.js
@@ -2,8 +2,10 @@ import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/reveal-on-scroll';
 import StickyHeader from './modules/StickyHeader'; 
 import Modal from './modules/Modal'; 
+import MenuBar from './modules/MenuBar';
 
 var mobileMenu = new MobileMenu(); /* var = object = new = person class -- creates new instance of the class */
 var revealOnScroll = new RevealOnScroll();
 var stickyHeader = new StickyHeader();
 var modal = new Modal();
+var MenuBar = new MenuBar(); 
